e4f2f08a6c3a0916a7b678da405e188103a27123
LukeSometimesCodes <121363338+LukeSometimesCodes@users.noreply.github.com>
2023-01-14 19:20:37 -0700

    First experiments with Javascript

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+var A = "This is a string";
+document.write(A);
+
+var B = "This is my test note";
+window.alert(B);
+
+document.write("Then I said to the guy, \"Biker? I hardly know her!\"");
+
+document.write("\"Be who you are and say what you feel,"+"because those who mind don\'t matter and those who matter don\'t mind.\""+"-Dr. Seuss");
+
+var C = ("It takes so much effort"+"To produce a smile these days");
+document.write(C);
+
+var family = "The Morreales", Dad ="Steve", Mom="Berlinda", Son="Luke";
+document.write(Son);
